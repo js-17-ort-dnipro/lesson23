fa060ef880a27c9117706e49134eb9eb11d1e2cd
anatoliy.kigel@gmail.com <Anatoliy Kigel>
2023-07-26 20:39:29 +0300

    Update

---
+++ src/demo-template/assets/js/app.js
@@ -0,0 +1,12 @@
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+   
+    // const firebaseConfig = {
+    //     ...Insert Keys Here...
+    // };
+
+    const app = initializeApp(firebaseConfig);
+
+    const db = getFirestore(app);
